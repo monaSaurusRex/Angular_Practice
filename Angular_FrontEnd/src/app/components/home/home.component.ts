28ff181eaa0b46788de65c7e84c88030376c652e
monaSaurusRex <monqiquenagel24.mn@gmail.com>
2023-04-18 16:28:06 +0200

    Created an interface and worked on converting the ui to a tabe format.

--- Angular_FrontEnd/src/app/components/home/home.component.ts
+++ Angular_FrontEnd/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FakestoreService } from 'src/app/services/fakestore.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +9,22 @@ import { FakestoreService } from 'src/app/services/fakestore.service';
 })
 export class HomeComponent implements OnInit{
 
-  constructor(private fakestore:FakestoreService){}
+  constructor(private fakestore:FakestoreService, private router:Router){} //inject the service you want to use
 
-  products:any[]=[];
+  products:any[]=[]; //values that you will be using 
+  //products:Product[] = []; 
 
   ngOnInit(){
-    this.fakestore.getProduct()
-      .subscribe((products:any)=>{
+    this.fakestore.getAll()
+      .subscribe((products:any)=>{ //arrow functions are already in the format of a promise
       console.table(products);
       this.products=products;
     })
+  } 
+
+  goToCreate(){
+    this.router.navigate(['/create']);
   }
+
 
 }
--- Angular_FrontEnd/src/app/services/fakestore.service.ts
+++ Angular_FrontEnd/src/app/services/fakestore.service.ts
@@ -2,16 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FakestoreService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  getAll() {
+    return this.http.get('https://fakestoreapi.com/products');
+  }
 
-  getProduct(){
-    return this.http.get('https://fakestoreapi.com/products')
-    // fetch('https://fakestoreapi.com/products')
-    //         .then(res=>res.json())
-    //         .then(json=>console.log(json))
+  getOneProduct(id: number) {
+    return this.http.get('' + id);
+  }
+
+  createProduct(body: any) {
+    //body means object -> any will eventually be replaced by an Object
+    return this.http.post('', body);
+  }
+
+  // createProduct(body:Product){ //body means object
+  //   this.http.post("",body);
+  // }
+
+  updateProduct(id: number, body: any) {
+    //combination of getOne and create
+    return this.http.put('' + id, body);
+  }
+
+  deleteProduct(id: number) {
+    //combination of getOne and create
+    return this.http.delete('' + id);
   }
 }
