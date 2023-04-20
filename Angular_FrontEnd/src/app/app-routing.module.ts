ecef08a5a7481f92292e7867ee15cb3b0eca288b
monaSaurusRex <monqiquenagel24.mn@gmail.com>
2023-04-20 13:59:53 +0200

    Created component for creating new product.

--- Angular_FrontEnd/src/app/app-routing.module.ts
+++ Angular_FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
 
 const routes: Routes = [
-  {path:"", component:HomeComponent}
-
+  {path:"", component:HomeComponent},
+  {path:"", component:CreateComponent}
 ];
 
 @NgModule({
--- Angular_FrontEnd/src/app/app.module.ts
+++ Angular_FrontEnd/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CreateComponent } from './components/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CreateComponent
   ],
   imports: [
     HttpClientModule,
